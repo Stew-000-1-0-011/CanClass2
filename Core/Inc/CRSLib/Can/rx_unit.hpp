89c4d27e7079c34f5c1cbdcd027078f1017386f5
Stew-000-1-0-011 <92617876+Stew-000-1-0-011@users.noreply.github.com>
2022-09-28 14:00:51 +0900

    コミット ──6392個のコンパイルエラーを沿えて──

--- Core/Inc/CRSLib/Can/rx_unit.hpp
+++ Core/Inc/CRSLib/Can/rx_unit.hpp
@@ -10,6 +10,7 @@
 #include <CRSLib/std_int.hpp>
 #include <CRSLib/utility.hpp>
 #include <CRSLib/debug.hpp>
+#include <CRSLib/executor.hpp>
 
 #include "utility.hpp"
 #include "offset_id.hpp"
@@ -23,22 +24,18 @@ namespace CRSLib::Can::Implement
 	template<IsOffsetIdsEnum OffsetIdsEnum_>
 	class RxUnit final : public UnitBase<OffsetIdsEnum_>
 	{
-		FifoIndex fifo_index;
 		RxIdTuple<OffsetIdsEnum::n> rx_ids{};
 
 	public:
-		RxUnit(const u32 base_id, const FifoIndex fifo_index) noexcept:
-			UnitBase<OffsetIdsEnum_>{base_id},
-			fifo_index{fifo_index}
+		RxUnit(const u32 base_id) noexcept:
+			UnitBase<OffsetIdsEnum_>{base_id}
 		{}
 
 	public:
-		// 受け取ったrx_frameを受け取るidがあれば, そのidのキューに格納する. キューに格納したらtrue, しなかったらfalseを返す.
-		bool push_queue(const RxFrame& rx_frame) noexcept
+		template<size_t queue_size>
+		void receive(const RxFrame& rx_frame, Executor<void () noexcept, queue_size>& executor) noexcept
 		{
-			bool is_pushed{false};
-
-			auto for_body_par_offset_id = [this, &is_pushed]<std::underlying_type_t<OffsetIdsEnum> offset_id>(CompileForIndex<offset_id>) noexcept
+			auto for_body_par_offset_id = [this]<std::underlying_type_t<OffsetIdsEnum> offset_id>(CompileForIndex<offset_id>) noexcept
 			{
 				CompileForIndex<offset_id + 1> ret{};
 				if constexpr(offset_id == to_underlying(OffsetIdsEnum::n))
@@ -50,7 +47,7 @@ namespace CRSLib::Can::Implement
 				if(rx_frame.header.id == base_id + offset_id)
 				{
 					std::get<offset_id>(rx_ids).queue.push(rx_frame);
-					is_pushed = true;
+					executor.queue.push(&std::get<offset_id>(rx_ids));
 					ret.is_breaked = true;
 					return ret;
 				}
@@ -59,41 +56,6 @@ namespace CRSLib::Can::Implement
 			}
 
 			compile_for(for_body_par_offset_id, CompileForIndex<to_underlying<OffsetIdsEnum>(0)>{});
-			return is_pushed;
-		}
-
-		// 一つ以上フレームを格納しているrx_idがあれば, そのコールバックを1つ呼び出す.
-		// コールバックを1つ呼び出せばtrueを、1つも呼び出さなければfalseを返す.
-		bool call_once() noexcept
-		{
-			bool is_called{false};
-
-			auto for_body_par_offset_id = [this, &is_called]<std::underlying_type_t<OffsetIdsEnum> offset_id>(CompileForIndex<offset_id>) noexcept
-			{
-				CompileForIndex<offset_id + 1> ret{};
-				if constexpr(offset_id == to_underlying(OffsetIdsEnum::n))
-				{
-					ret.is_breaked = true;
-					return ret;
-				}
-
-				if(std::get<offset_id>(rx_ids).call_once())
-				{
-					is_called = true;
-					ret.is_breaked = true;
-					return ret;
-				}
-
-				return ret;
-			};
-
-			compile_for(for_body_par_offset_id, CompileForIndex<to_underlying<OffsetIdsEnum>(0)>{});
-			return is_called;
-		}
-
-		FilterId get_filter_id() const noexcept
-		{
-			return {.base_id = base_id, .align = align, .fifo_index = fifo_index};
 		}
 	};
 }
